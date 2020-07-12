4ba00518499d5bf060720ee27ebbd3ddbb7fe60c
Olivier Castanos <olivier@MacBook-Pro.local>
2020-07-12 12:23:31 +0200

    polish

--- masternode/install.sh
+++ masternode/install.sh
@@ -17,8 +17,14 @@ systemctl enable docker &&  systemctl start docker
 yum install kubeadm -y
 systemctl enable kubelet && systemctl start kubelet
 swapoff -a
-kubeadm init
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+
+kubeadm init   --v=5
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+
+
+ 
