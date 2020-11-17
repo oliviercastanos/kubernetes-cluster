1dca2fba3bf35320362dd05c51a407d3e7749828
Olivier Castanos <olivier@castanos.pro>
2020-11-17 15:00:12 +0100

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -2,6 +2,5 @@ kubeadm reset -f --v=5
 rm /etc/cni/net.d -rfv
 rm  /etc/kubernetes/manifests/* -rfv
 rm .kube/ -rfv
-  echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 swapoff -a
-
