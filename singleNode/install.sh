fd90ed72316eb15e56bb0c96899e14425414ec00
Olivier Castanos <olivier@castanos.fr>
2021-02-19 21:11:23 +0100

    update

--- singleNode/install.sh
+++ singleNode/install.sh
@@ -10,7 +10,7 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
-yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine -y
+yum remove runc podman docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine -y
 curl https://download.docker.com/linux/centos/docker-ce.repo > /etc/yum.repos.d/docker.repo
 yum install docker-ce docker-ce-cli containerd.io -y
 systemctl enable docker &&  systemctl start docker
