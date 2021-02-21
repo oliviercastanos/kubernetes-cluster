4b34e52e60a239d170a15f0a598dedc2ec68e015
Olivier Castanos <olivier@castanos.fr>
2021-02-21 19:47:03 +0100

    Update ingresscontroller.sh

--- singleNode/ingresscontroller.sh
+++ singleNode/ingresscontroller.sh
@@ -1,3 +1,7 @@
 kubectl taint nodes --all node-role.kubernetes.io/master-
-kubectl apply -f https://raw.githubusercontent.com/oliviercastanos/kubernetes-cluster/master/singleNode/haproxy-ingress.yaml
-kubectl label node $(hostname) role=ingress-controller
+kubectl create namespace ingress-controller
+helm repo add haproxytech https://haproxytech.github.io/helm-charts
+helm repo update
+helm install haproxy haproxytech/kubernetes-ingress --set controller.kind=DaemonSet --set controller.daemonset.useHostPort=true  -n ingress-controller
+
+
